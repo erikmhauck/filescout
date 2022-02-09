1ec38f760eeacef1839f5a59900795f038503ea2
Erik Hauck <erikmhauck@gmail.com>
2022-02-08 23:39:17 -0600

    move webpack to root

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 "use strict";
 
-const express = require("express");
+import express from "express";
 
 const PORT = 8080;
-const HOST = "0.0.0.0";
+const HOST = "localhost";
 
 const app = express();
+
 app.get("/api/hello", (req, res) => {
   res.send("Hello World API");
 });
-app.use(express.static("client"));
+app.use(express.static("dist"));
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
