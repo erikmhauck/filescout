9db18c70a6e589be82011dca3c68762b4ab574da
Erik Hauck <erikmhauck@gmail.com>
2022-02-21 11:17:52 -0600

    add loading icon to table

--- client/components/search/results.tsx
+++ client/components/search/results.tsx
@@ -6,26 +6,27 @@ import { DataGrid, GridColDef } from '@mui/x-data-grid';
 const log = logger('results');
 
 const columns: GridColDef[] = [
-  { field: 'path', headerName: 'File path', width: 130 },
-  { field: 'contents', headerName: 'Contents', width: 130 },
+  { field: 'path', headerName: 'File' },
+  { field: 'contents', headerName: 'Contents' },
 ];
 
 interface ResultsProps {
   results: FileDocument[];
+  loading: boolean;
 }
 
-export const Results = ({ results }: ResultsProps) => {
+export const Results = ({ results, loading }: ResultsProps) => {
   log.info(`Rendering ${results.length} search results!`);
 
   return (
     <div style={{ display: 'flex', height: '100%' }}>
       <div style={{ flexGrow: 1 }}>
         <DataGrid
+          loading={loading}
           rows={results}
           columns={columns}
           pageSize={10}
           rowsPerPageOptions={[10]}
-          checkboxSelection
           getRowId={(row) => row._id}
         />
       </div>
--- client/components/search/searchBar.tsx
+++ client/components/search/searchBar.tsx
@@ -11,8 +11,10 @@ const log = logger('search');
 
 const useSearch = () => {
   const [results, setResults] = React.useState<FileDocument[]>([]);
+  const [loading, setLoading] = React.useState<boolean>(false);
 
   const executeSearch = async (query: string) => {
+    setLoading(true);
     const queryBody = JSON.stringify({ query: query });
     log.info(`searching: ${queryBody}`);
     const response = await fetch('/api/search', {
@@ -23,6 +25,7 @@ const useSearch = () => {
       body: queryBody,
     });
     const responseJson: FileDocument[] = await response.json();
+    setLoading(false);
     if (responseJson.length > 0) {
       setResults(responseJson);
     } else {
@@ -30,12 +33,12 @@ const useSearch = () => {
     }
   };
 
-  return { results, executeSearch };
+  return { results, executeSearch, loading };
 };
 
 export const SearchBar = ({}: SearchProps) => {
   const [queryString, setQueryString] = React.useState('');
-  const { results, executeSearch } = useSearch();
+  const { results, executeSearch, loading } = useSearch();
 
   return (
     <>
@@ -59,7 +62,7 @@ export const SearchBar = ({}: SearchProps) => {
           <Button>go</Button>
         </Grid>
       </Grid>
-      <Results results={results} />
+      <Results results={results} loading={loading} />
     </>
   );
 };
